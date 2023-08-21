231857b41552fa1cd09caf3c8849b7c9b83a55af
Arowa Suliman <arowa@chromium.org>
2023-08-21 10:57:08 +0300

    wifi: ath11k: Add crash logging
    Currently, the ath11k driver does not print a crash signature when a
    crash happens. Checked by triggering a simulated crash using the command
    [1] and checking dmesg for logs.
    
    [1] echo assert > /sys/kernel/debug/ath11k/../simulate_fw_crash
    
    Change the crash logging from debug to informational and add a warning
    when firmware crash MHI_CB_EE_RDDM happens.
    
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-03125-QCAHSPSWPL_V1_V2_SILICONZ_LITE-3.6510.23
    
    Signed-off-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Patchwork-Id: 13312801
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath11k/mhi.c
+++ drivers/net/wireless/ath/ath11k/mhi.c
@@ -325,14 +325,15 @@ static void ath11k_mhi_op_status_cb(struct mhi_controller *mhi_cntrl,
 {
 	struct ath11k_base *ab = dev_get_drvdata(mhi_cntrl->cntrl_dev);
 
-	ath11k_dbg(ab, ATH11K_DBG_BOOT, "notify status reason %s\n",
-		   ath11k_mhi_op_callback_to_str(cb));
+	ath11k_info(ab, "notify status reason %s\n",
+		    ath11k_mhi_op_callback_to_str(cb));
 
 	switch (cb) {
 	case MHI_CB_SYS_ERROR:
 		ath11k_warn(ab, "firmware crashed: MHI_CB_SYS_ERROR\n");
 		break;
 	case MHI_CB_EE_RDDM:
+		ath11k_warn(ab, "firmware crashed: MHI_CB_EE_RDDM\n");
 		if (!(test_bit(ATH11K_FLAG_UNREGISTERING, &ab->dev_flags)))
 			queue_work(ab->workqueue_aux, &ab->reset_work);
 		break;
