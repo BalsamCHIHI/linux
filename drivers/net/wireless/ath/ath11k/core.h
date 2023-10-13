d0f5e18f1a992c7df13901332aebff01d98526ea
Jeff Johnson <quic_jjohnson@quicinc.com>
2023-10-13 07:24:08 -0700

    wifi: ath11k: Remove ath11k_base::bd_api
    Currently struct ath11k_base defines the member bd_api. However, this
    member is only accessed within ath11k_core_fetch_bdf(). Since the
    scope is local just to that one function, remove it from ath11k_base
    and instead just use a local stack variable.
    
    No functional changes, compile tested only.
    
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Patchwork-Id: 13420964
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath11k/core.h
+++ drivers/net/wireless/ath/ath11k/core.h
@@ -909,7 +909,6 @@ struct ath11k_base {
 	struct ath11k_targ_cap target_caps;
 	u32 ext_service_bitmap[WMI_SERVICE_EXT_BM_SIZE];
 	bool pdevs_macaddr_valid;
-	int bd_api;
 
 	struct ath11k_hw_params hw_params;
 
