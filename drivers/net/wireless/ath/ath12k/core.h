5085843538af88572c043cf618e87765895e6ea8
Lingbo Kong <quic_lingbok@quicinc.com>
2024-04-22 17:00:43 +0300

    wifi: ath12k: report station mode signal strength
    Currently, the signal strength of "iw dev xxx station dump" always show an
    invalid value.
    
    This is because signal strength is only set in ath12k_mgmt_rx_event()
    function, and not set for received data packet. So, change to get signal
    from firmware and report to mac80211.
    
    After that, "iw dev xxx station dump" show the correct signal strength.
    Such as:
    
    Station 00:03:7f:12:03:03 (on wlo1)
            inactive time:  36 ms
            rx bytes:       61571
            rx packets:     336
            tx bytes:       28204
            tx packets:     205
            tx retries:     49
            tx failed:      0
            beacon loss:    0
            beacon rx:      83
            rx drop misc:   66
            signal:         -24 dBm
            beacon signal avg:      -22 dBm
    
    For WCN7850, the firmware supports db2dbm, so not need to add noise floor.
    For QCN9274, the firmware not support db2dbm, so need to add noise floor.
    
    This patch affects the station mode of WCN7850 and QCN9274.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.2.1-00201-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Lingbo Kong <quic_lingbok@quicinc.com>
    Patchwork-Id: 13635583
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -451,6 +451,7 @@ struct ath12k_sta {
 	struct ath12k_wbm_tx_stats *wbm_tx_stats;
 	u32 bw_prev;
 	u32 peer_nss;
+	s8 rssi_beacon;
 };
 
 #define ATH12K_MIN_5G_FREQ 4150
@@ -634,6 +635,8 @@ struct ath12k {
 
 	u32 freq_low;
 	u32 freq_high;
+
+	struct completion fw_stats_complete;
 };
 
 struct ath12k_hw {
