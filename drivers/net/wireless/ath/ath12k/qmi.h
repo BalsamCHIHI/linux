1d73ad354d018057c14e95e892433f89f336ac85
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-04-25 12:53:22 +0530

    wifi: ath12k: Send QMI host capability after device group is ready
    QMI host capability has the information regarding MLO parameters such as
    device_id, MLO capability, group id and the information of each devices
    involved in the group and sent immediately on QMI server arrive event.
    Currently, only one device is involved in the group and hence, single
    device information is sent as part of MLO capability of host.
    
    But, in future when multi device group abstraction is introduced, host
    should send all the device information involved in the group as part
    of QMI MLO host capability rather than single device. Hence, sending
    QMI host capability immediately on server arrive of a device might not
    be ideal for multi device group abstraction as the details of other
    devices in the group would not be available.
    
    Hence, once QMI server arrive event is received, request for QMI phy
    capabilities of device, and defer the host capability send for that device.
    After QMI phy capability is received for all the devices in the group
    trigger the host capability event for the deferred devices in the group.
    
    Hence, add changes to defer the QMI host capability event until the device
    group is ready and then resume the QMI exchange for all the device with
    host capabilities.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.0.1-00029-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Signed-off-by: Harshitha Prem <quic_hprem@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/qmi.h
+++ drivers/net/wireless/ath/ath12k/qmi.h
@@ -68,6 +68,7 @@ enum ath12k_qmi_event_type {
 	ATH12K_QMI_EVENT_FORCE_FW_ASSERT,
 	ATH12K_QMI_EVENT_POWER_UP,
 	ATH12K_QMI_EVENT_POWER_DOWN,
+	ATH12K_QMI_EVENT_HOST_CAP,
 	ATH12K_QMI_EVENT_MAX,
 };
 
@@ -142,6 +143,7 @@ struct ath12k_qmi {
 	u32 target_mem_mode;
 	bool target_mem_delayed;
 	u8 cal_done;
+	bool block_event;       /* protect by event spinlock */
 	u8 num_radios;
 	struct target_info target;
 	struct m3_mem_region m3_mem;
@@ -600,5 +602,20 @@ void ath12k_qmi_firmware_stop(struct ath12k_base *ab);
 void ath12k_qmi_deinit_service(struct ath12k_base *ab);
 int ath12k_qmi_init_service(struct ath12k_base *ab);
 void ath12k_qmi_free_resource(struct ath12k_base *ab);
+void ath12k_qmi_trigger_host_cap(struct ath12k_base *ab);
+
+static inline void ath12k_qmi_set_event_block(struct ath12k_qmi *qmi, bool block)
+{
+	lockdep_assert_held(&qmi->event_lock);
+
+	qmi->block_event = block;
+}
+
+static inline bool ath12k_qmi_get_event_block(struct ath12k_qmi *qmi)
+{
+	lockdep_assert_held(&qmi->event_lock);
+
+	return qmi->block_event;
+}
 
 #endif
