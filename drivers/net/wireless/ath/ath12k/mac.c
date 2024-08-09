1de4777d375ac593ea24af80e47e1acb3a9559a8
Sriram R <quic_srirrama@quicinc.com>
2024-08-09 15:41:37 +0530

    wifi: ath12k: update ath12k_mac_op_conf_tx for MLO
    Refactor ath12k_mac_op_conf_tx() to apply/cache the TX
    parameters based on link id provided by mac80211.
    
    While at it the link id argument of ath12k_mac_conf_tx() is
    not really used as the corresponding link arvif itself is
    now passed to it so remove the same.
    
    Signed-off-by: Sriram R <quic_srirrama@quicinc.com>
    Co-developed-by: Rameshkumar Sundaram <quic_ramess@quicinc.com>
    Signed-off-by: Rameshkumar Sundaram <quic_ramess@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -5153,8 +5153,7 @@ exit:
 	return ret;
 }
 
-static int ath12k_mac_conf_tx(struct ath12k_link_vif *arvif,
-			      unsigned int link_id, u16 ac,
+static int ath12k_mac_conf_tx(struct ath12k_link_vif *arvif, u16 ac,
 			      const struct ieee80211_tx_queue_params *params)
 {
 	struct wmi_wmm_params_arg *p = NULL;
@@ -5215,15 +5214,16 @@ static int ath12k_mac_op_conf_tx(struct ieee80211_hw *hw,
 	struct ath12k_hw *ah = ath12k_hw_to_ah(hw);
 	struct ath12k_link_vif *arvif;
 	struct ath12k_vif_cache *cache;
-	struct ath12k *ar;
 	int ret;
 
+	if (link_id >= IEEE80211_MLD_MAX_NUM_LINKS)
+		return -EINVAL;
+
 	mutex_lock(&ah->conf_mutex);
-	arvif = &ahvif->deflink;
-	ar = ath12k_get_ar_by_vif(hw, vif);
-	if (!ar) {
-		/* cache the info and apply after vdev is created */
-		cache = ath12k_ahvif_get_link_cache(ahvif, ATH12K_DEFAULT_LINK_ID);
+	arvif = rcu_dereference_protected(ahvif->link[link_id],
+			lockdep_is_held(&ah->conf_mutex));
+	if (!arvif || !arvif->is_created) {
+		cache = ath12k_ahvif_get_link_cache(ahvif, link_id);
 		if (!cache) {
 			mutex_unlock(&ah->conf_mutex);
 			return -ENOSPC;
@@ -5235,9 +5235,9 @@ static int ath12k_mac_op_conf_tx(struct ieee80211_hw *hw,
 		return 0;
 	}
 
-	mutex_lock(&ar->conf_mutex);
-	ret = ath12k_mac_conf_tx(arvif, link_id, ac, params);
-	mutex_unlock(&ar->conf_mutex);
+	mutex_lock(&arvif->ar->conf_mutex);
+	ret = ath12k_mac_conf_tx(arvif, ac, params);
+	mutex_unlock(&arvif->ar->conf_mutex);
 	mutex_unlock(&ah->conf_mutex);
 	return ret;
 }
@@ -6986,7 +6986,7 @@ static void ath12k_mac_vif_cache_flush(struct ath12k *ar, struct ath12k_link_vif
 		return;
 
 	if (cache->tx_conf.changed) {
-		ret = ath12k_mac_conf_tx(arvif, 0, cache->tx_conf.ac,
+		ret = ath12k_mac_conf_tx(arvif, cache->tx_conf.ac,
 					 &cache->tx_conf.tx_queue_params);
 		if (ret)
 			ath12k_warn(ab,
