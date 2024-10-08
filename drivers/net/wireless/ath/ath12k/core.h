52adee948fce8e82c642ceb1ad564084335a03c8
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-10-08 16:53:58 +0300

    wifi: ath12k: Add support to allocate MLO global memory region
    To enable Multi Link Operation (MLO), QCN9274 firmware requests MLO
    global memory (MLO_GLOBAL_MEM_REGION_TYPE). This memory region is
    shared across all the firmware (SoC) that are participation in the
    MLO.
    
    Hence, add support to allocate and free MLO global memory region.
    Allocate one MLO global memory per ath12k_hw_group and assign the same
    memory to all firmware in the same ath12k_hw_group. WCN7850 firmware
    does not request this memory type, therefore this change will have no
    impact on WCN7850 device.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.1.1-00210-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Co-developed-by: Raj Kumar Bhagat <quic_rajkbhag@quicinc.com>
    Signed-off-by: Raj Kumar Bhagat <quic_rajkbhag@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -813,6 +813,12 @@ struct ath12k_soc_dp_stats {
 	struct ath12k_soc_dp_tx_err_stats tx_err;
 };
 
+struct ath12k_mlo_memory {
+	struct target_mem_chunk chunk[ATH12K_QMI_WLANFW_MAX_NUM_MEM_SEG_V01];
+	int mlo_mem_size;
+	bool init_done;
+};
+
 /* Holds info on the group of devices that are registered as a single wiphy */
 struct ath12k_hw_group {
 	struct list_head list;
@@ -842,6 +848,7 @@ struct ath12k_hw_group {
 	struct ath12k_hw *ah[ATH12K_GROUP_MAX_RADIO];
 	u8 num_hw;
 	bool mlo_capable;
+	struct ath12k_mlo_memory mlo_mem;
 };
 
 #define ATH12K_WSI_MAX_ADJACENT_DEVICES   2
