b092c9852ca254dd9a4c57285b2fd9c70ea1db55
Kalle Valo <quic_kvalo@quicinc.com>
2024-11-04 20:03:56 +0200

    wifi: ath12k: mac: use lockdep_assert_wiphy() more
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -3032,6 +3032,8 @@ static void ath12k_peer_assoc_h_mlo(struct ath12k_link_sta *arsta,
 	u8 link_id;
 	int i;
 
+	lockdep_assert_wiphy(ahsta->ahvif->ah->hw->wiphy);
+
 	if (!sta->mlo || ahsta->ml_peer_id == ATH12K_MLO_PEER_ID_INVALID)
 		return;
 
@@ -6900,6 +6902,8 @@ static void ath12k_mgmt_over_wmi_tx_work(struct wiphy *wiphy, struct wiphy_work 
 	struct ath12k_link_vif *arvif;
 	struct sk_buff *skb;
 	int ret;
+
+	lockdep_assert_wiphy(wiphy);
 
 	while ((skb = skb_dequeue(&ar->wmi_mgmt_tx_queue)) != NULL) {
 		skb_cb = ATH12K_SKB_CB(skb);
