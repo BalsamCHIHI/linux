c2eae29d1eaf855d69910695de7098fdd1c518dd
Kiran Venkatappa <quic_kiranv@quicinc.com>
2025-03-28 00:53:58 +0530

    wifi: ath12k: Modularize ath12k into shared HW dep comp
    Modularize ath12k driver into two kos. ath12k.ko and ath12k_wifi7.ko.
    ath12k.ko includes shared code that can be leveraged and
    ath12k_wifi7.ko includes wifi7 device specific code.
    With this, ath12k.ko is common module and it needs to loaded before
    loading device specific module. Device specific module is dependent
    on common module.
    Previous patches in the series restructured some code as device
    specific files. This patch moves wifi7 device family files into new
    wifi7 directory and builds them as separate ko and exports common
    symbols accordingly. Some of the exported symbols will be modified
    as next set of patches moves some more code into architecture
    dependent code.
    
                                              +-----------------+
                                              |                 |
                                              |   ath12k.ko     |
                                              |    (common)     |
            +---------------+                 |                 |
            |               |                 +-----------------+
            |   ath12k.ko   | ===========>
            |               |                 +------------------+
            +---------------+                 |                  |
                                              | ath12k_wifi7.ko  |
                                              | (wifi7 family)   |
                                              |                  |
                                              +------------------+
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    Tested-on: IPQ5332 hw1.0 AHB WLAN.WBE.1.3.1-00130-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Kiran Venkatappa <quic_kiranv@quicinc.com>
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/ahb.c
+++ drivers/net/wireless/ath/ath12k/ahb.c
@@ -1181,6 +1181,7 @@ int ath12k_ahb_register_driver(const enum ath12k_device_family device_id,
 
 	return platform_driver_register(ahb_driver);
 }
+EXPORT_SYMBOL(ath12k_ahb_register_driver);
 
 void ath12k_ahb_unregister_driver(const enum ath12k_device_family device_id)
 {
@@ -1196,3 +1197,4 @@ void ath12k_ahb_unregister_driver(const enum ath12k_device_family device_id)
 	platform_driver_unregister(ahb_driver);
 	ath12k_ahb_family_drivers[device_id] = NULL;
 }
+EXPORT_SYMBOL(ath12k_ahb_unregister_driver);
