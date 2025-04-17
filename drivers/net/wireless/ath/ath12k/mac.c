e63983e12e0640547053ae8f1c7a6c8b07bf241a
Aditya Kumar Singh <aditya.kumar.singh@oss.qualcomm.com>
2025-04-17 09:00:24 +0530

    wifi: ath12k: fix ATH12K_FLAG_REGISTERED flag handling
    Commit a5686ae820fa ("wifi: ath12k: move ATH12K_FLAG_REGISTERED handling to
    ath12k_mac_register()") relocated the setting of the ATH12K_FLAG_REGISTERED
    flag to the ath12k_mac_register() function. However, this function only
    accesses the first device (ab) via ag->ab[0], resulting in the flag being
    set only for the first device in the group. Similarly,
    ath12k_mac_unregister() only unsets the flag for the first device. The flag
    should actually be set for all devices in the group to avoid issues during
    recovery.
    
    Hence, move setting and clearing of this flag in the function
    ath12k_core_hw_group_start() and ath12k_core_hw_group_stop() respectively.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.4.1-00199-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Fixes: a5686ae820fa ("wifi: ath12k: move ATH12K_FLAG_REGISTERED handling to ath12k_mac_register()")
    Signed-off-by: Aditya Kumar Singh <aditya.kumar.singh@oss.qualcomm.com>
    Reviewed-by: Vasanthakumar Thiagarajan <vasanthakumar.thiagarajan@oss.qualcomm.com>
    Signed-off-by: Raj Kumar Bhagat <quic_rajkbhag@quicinc.com>
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -12715,7 +12715,6 @@ void ath12k_mac_mlo_teardown(struct ath12k_hw_group *ag)
 
 int ath12k_mac_register(struct ath12k_hw_group *ag)
 {
-	struct ath12k_base *ab = ag->ab[0];
 	struct ath12k_hw *ah;
 	int i;
 	int ret;
@@ -12727,8 +12726,6 @@ int ath12k_mac_register(struct ath12k_hw_group *ag)
 		if (ret)
 			goto err;
 	}
-
-	set_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	return 0;
 
@@ -12746,11 +12743,8 @@ err:
 
 void ath12k_mac_unregister(struct ath12k_hw_group *ag)
 {
-	struct ath12k_base *ab = ag->ab[0];
 	struct ath12k_hw *ah;
 	int i;
-
-	clear_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	for (i = ag->num_hw - 1; i >= 0; i--) {
 		ah = ath12k_ag_to_ah(ag, i);
