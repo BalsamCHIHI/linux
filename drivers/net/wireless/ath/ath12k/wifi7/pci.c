ebca60f71c5f82752afc931344a2ec1160fe0032
Pavankumar Nandeshwar <quic_pnandesh@quicinc.com>
2025-05-16 12:53:05 +0530

    wifi: ath12k: Make compact tlv ops as the default ops for qcn
    Make compact tlv ops as the default ops by registering them in
    hw_params by default for qcn, and remove non-compact tlv ops and the
    corresponding hal APIs qcn, as they won't be in use for qcn.
    
    No changes for wcn hal ops as these changes are specific to qcn.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.4.1-00199-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Pavankumar Nandeshwar <quic_pnandesh@quicinc.com>
    Signed-off-by: Ripan Deuri <quic_rdeuri@quicinc.com>
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/wifi7/pci.c
+++ drivers/net/wireless/ath/ath12k/wifi7/pci.c
@@ -100,7 +100,7 @@ static int ath12k_wifi7_pci_probe(struct pci_dev *pdev,
 		ab_pci->msi_config = &ath12k_wifi7_msi_config[0];
 		ab->static_window_map = true;
 		ab_pci->pci_ops = &ath12k_wifi7_pci_ops_qcn9274;
-		ab->hal_rx_ops = &hal_rx_qcn9274_ops;
+		ab->hal_rx_ops = &hal_rx_qcn9274_compact_ops;
 		ath12k_wifi7_pci_read_hw_version(ab, &soc_hw_version_major,
 						 &soc_hw_version_minor);
 		switch (soc_hw_version_major) {
